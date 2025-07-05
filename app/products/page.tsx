9dfe521b4f19d965a3219adbd30269f3cff5b341
Hassan Moustafa <hassanmoustafa40@yahoo.com>
2025-07-05 19:37:29 +0300

    enhance search functionality

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -9,7 +9,7 @@ import NoFoundData from '../../components/no-found-data/NoFoundData';
 import './SearchBar.css'; // Assuming you have a CSS file for styling
 import { ChangeEvent, useState } from 'react';
 export default function ProductsPage() {
-  const { results, search, setSearch, loading } =
+  const { results, search, setSearch, loading, noResults } =
     useSearchProducts(searchProducts);
   const [isSearchStarted, setSearchStartStatus] = useState(false);
 
@@ -25,7 +25,7 @@ export default function ProductsPage() {
     
 
   return (
-    <div className={`search-page-container row mx-0 justify-content-center align-content-center ${results.length > 0 ? 'py-5' : ''}`}>
+    <div className={`search-page-container row mx-0 justify-content-center  ${results.length > 0 ? 'py-5' : ''} ${loading === "LOADING_ENDED" && search.length > 0 && results.length == 0 ? 'py-5 align-content-start' : 'align-content-center'}`}>
       <div className="search-input-field col-12 col-sm-10 col-md-6">
         <label className="search-label">Search products by keyword</label>
         <div className="search-form">
@@ -40,15 +40,15 @@ export default function ProductsPage() {
             <Search className="search-icon" />
           </button>
         </div>
-        <div className="row mx-0 justify-content-center mt-3 px-0">
-            {loading && <p>Loading...</p>}
-            {!loading && search.length > 0 && (<span className='products-count-label px-0'>Total results count: <span className='products-count'>{results.length}</span></span>)}
-            {!loading && isSearchStarted && results.length == 0 ? <NoFoundData></NoFoundData>: ''}
+        <div className="row mx-0 justify-content-center mt-3 px-0" style={{ height: '50px'}}>
+            {loading === "LOADING_STARTED" && <p className='loading-label px-0'>Loading...</p>}
+            {loading === "LOADING_ENDED" && search.length > 0 && (<span className='products-count-label px-0'>Total results count: <span className='products-count'>{results.length}</span></span>)}
+            {loading === "LOADING_ENDED" && search.length > 0 && results.length == 0 ? <NoFoundData></NoFoundData>: ''}
         </div>
       </div>
 
 
-      <div className="product-grid row mx-0 justify-content-center mt-5 col-12 col-xl-10">
+      <div className="product-grid row mx-0 justify-content-center col-12 col-xl-10">
         {results.map((product) => (
           <div key={product.id} className='col-auto px-0'>
             <ProductCard {...product} />
--- hooks/useSearchProducts.ts
+++ hooks/useSearchProducts.ts
@@ -3,24 +3,38 @@ import { useEffect, useState } from 'react';
 import { useDebounce } from './useDebounce';
 
 export function useSearchProducts(fetchFn: (query: string) => Promise<any[]>) {
-  const [search, setSearch] = useQueryState('q', parseAsString.withDefault(''));
-  const [results, setResults] = useState<any[]>([]);
-  const [loading, setLoading] = useState(false);
+	const [search, setSearch] = useQueryState('q', parseAsString.withDefault(''));
+	const [results, setResults] = useState<any[]>([]);
+	const [loading, setLoading] = useState('NO_LOAD');
+	const [noResults, setNoResults] = useState(false);
 
-  const debouncedSearch = useDebounce(search, 500); 
+	const debouncedSearch = useDebounce(search, 500);
 
-  useEffect(() => {
-    if (!debouncedSearch) {
-      setResults([]);
-      return;
-    }
+	useEffect(() => {
+		if (!debouncedSearch) {
+			setResults([]);
+			return;
+		}
 
-    setLoading(true);
-    fetchFn(debouncedSearch)
-      .then(setResults)
-      .catch(console.error)
-      .finally(() => setLoading(false));
-  }, [debouncedSearch, fetchFn]);
+		// console.log('loading');
+		// setLoading(true);
+		fetchFn(debouncedSearch)
+			.then(setResults)
+			.catch(console.error)
+			.finally(() => {
+				setLoading('LOADING_ENDED');
+				// setSearchingStartStatus(false);
+			});
+	}, [debouncedSearch, fetchFn]);
 
-  return { results, search, setSearch, loading };
+	useEffect(() => {
+		if (search) {
+			setLoading('LOADING_STARTED');
+		} else {
+			setLoading('NO_LOAD');
+			// setSearchingStartStatus(false);
+		}
+	}, [search]);
+
+	return { results, search, setSearch, loading, noResults };
 }
